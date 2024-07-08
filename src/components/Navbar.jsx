894cfcb4726871b219ac4b4f7b9a96da6078b83a
Ahmed Masada <121398355+AhmedRmies@users.noreply.github.com>
2024-07-08 14:58:55 +0800

    deploy config

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="container">
-        <a href="/" className="logo">CetakWorks</a>
+        <a href="#design" className="logo">CetakWorks</a>
         <ul className="nav-links">
           <li><a href="#design" className="btn-design">Design Your Shirt</a></li>
         </ul>
